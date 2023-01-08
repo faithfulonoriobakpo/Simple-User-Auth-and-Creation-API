3f6b546ba779d4ed9f42a3f3fc4f80385a66c95b
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2023-01-08 13:01:44 +0100

    Added user login route

--- src/routes/api/users.ts
+++ src/routes/api/users.ts
@@ -1,4 +1,13 @@
-import express from "express";
+import express, {Request, Response} from "express";
 
 const users = express.Router();
+
+users.post('/login', (req:Request, res:Response) => {
+    const data = req.body;
+    try {
+
+    } catch(err) {
+        
+    }
+});
 
