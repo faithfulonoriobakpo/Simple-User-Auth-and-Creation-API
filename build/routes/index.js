a96bb62b7926fc6aec2ed87ae9c4729ed7304fbe
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2023-02-05 08:40:22 +0100

    built latest changes

--- build/routes/index.js
+++ build/routes/index.js
@@ -7,4 +7,14 @@ const express_1 = __importDefault(require("express"));
 const users_1 = __importDefault(require("./api/users"));
 const routes = express_1.default.Router();
 routes.use('/api', users_1.default);
+routes.use("*", (req, res) => {
+    res.status(404).json({
+        success: "false",
+        message: "Page not found",
+        error: {
+            statusCode: 404,
+            message: "You reached a route that is not defined on this server",
+        },
+    });
+});
 exports.default = routes;
