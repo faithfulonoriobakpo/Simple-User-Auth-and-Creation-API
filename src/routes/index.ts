1884432c20d2bca2e185346ae6ad64e1e4a2ca3f
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2023-02-05 08:39:31 +0100

    added a universal page not found route for undefined routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,4 +5,15 @@ const routes = express.Router();
 
 routes.use('/api', users);
 
+routes.use("*", (req, res) => {
+    res.status(404).json({
+      success: "false",
+      message: "Page not found",
+      error: {
+        statusCode: 404,
+        message: "You reached a route that is not defined on this server",
+      },
+    });
+  });
+
 export default routes;
